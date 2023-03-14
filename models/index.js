3ffa31b4830543ce3e70ad5ce6cfbec50c0dea4a
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-03-13 20:35:16 -0400

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,8 +1,29 @@
 const User = require("./User");
+const Book = require("./Book");
+const Review = require("./Review");
 
-User.hasMany(Project, {
+// // Defines a User as having many Reviews, thus creating a foreign key in the `Review` table
+User.hasMany(Review, {
   foreignKey: "user_id",
   onDelete: "CASCADE",
 });
 
-module.exports = { User };
+// Defines a Book as having many Reviews, thus creating a foreign key in the `Review` table
+Book.hasMany(Review, {
+  foreignKey: "book_id",
+  onDelete: "CASCADE",
+});
+
+// A review is owned by a user
+// The association between a User and a Review can also be created from the Review side
+Review.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+// A review is owned by a book
+// The association between a Book and a Review can also be created from the Reviews side
+Review.belongsTo(Book, {
+  foreignKey: "book_id",
+});
+
+module.exports = { User, Book, Review };
