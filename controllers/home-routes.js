8b6f0eec866ed74309875087d67d4e82187fa0dc
Jags <jon.kmjt@gmail.com>
2023-03-15 19:41:30 -0400

    book data and review data visible in corresponding hbs files

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,41 @@
 const router = require("express").Router();
+const { User, Book, Review } = require("../models");
 
-// route to get homepage
+// route to get all books: this will be the library page eventually
 router.get("/", async (req, res) => {
-  res.render("home.handlebars");
+  try {
+    const bookData = await Book.findAll();
+
+    const books = bookData.map((book) => book.get({ plain: true }));
+
+    res.render("home", {
+      books,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+//will render page for individual book and its corresponding review
+router.get("/book/:id", async (req, res) => {
+  try {
+    const dbBookData = await Book.findByPk(req.params.id, {
+      include: [
+        {
+          model: Review,
+          attributes: ["review", "user_id"],
+        },
+      ],
+    });
+
+    const book = dbBookData.get({ plain: true });
+    console.log(book);
+    res.render("bookPage", { book });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
